bbfc7f5a79285f2dc0551fed1ccf5e0c9183f082
Nai Xia <nai.xia@gmail.com>
2011-03-11 22:19:32 +0800

    try to change kmem_cache_alloc to kmem_cache_zalloc for VMA cache

--- kernel/fork.c
+++ kernel/fork.c
@@ -350,7 +350,7 @@ static int dup_mmap(struct mm_struct *mm, struct mm_struct *oldmm)
 				goto fail_nomem;
 			charge = len;
 		}
-		tmp = kmem_cache_alloc(vm_area_cachep, GFP_KERNEL);
+		tmp = kmem_cache_zalloc(vm_area_cachep, GFP_KERNEL);
 		if (!tmp)
 			goto fail_nomem;
 		*tmp = *mpnt;
